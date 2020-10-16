926fa04ace3eb02e084ae14026f47fe93d8fc2ab
Ruiz Sanchez <ruizcayetano@hotmail.com>
2020-10-15 22:40:17 -0600

    deploying to github

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import React from "react";
+import Employee from "./pages/Employee";
+// import "./App.css";
+import "./index.css";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="jumotron">
+                Staff Directory
+      </header>
+            <Employee />
+        </div>
+    );
+}
+
+export default App;
